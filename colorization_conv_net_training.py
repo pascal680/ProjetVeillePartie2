a663e9268b49e0af54c6186fa3d0ffd76f486885
Pascal Bourgoin <pascal680@hotmail.ca>
2021-11-09 03:49:25 -0500

    Essais #3 de modele de reseau neuronale conv avec des couches conv de largeur differente pour optimiser entrainement.

--- colorization_conv_net_training.py
+++ colorization_conv_net_training.py
@@ -6,24 +6,28 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D,
 import pickle
 
 X = pickle.load(open("X.pickle", "rb"))
-X = np.array(X).reshape(-1, 64, 64, 1)
 Y = pickle.load(open("Y.pickle", "rb"))
-Y = np.array(Y)
 
-X = X/255.0 #can use keras.utils.normalize
+# X = X/255.0 #can use keras.utils.normalize
 
 
 # building a linear stack of layers with the sequential model
 model = Sequential()
 
-model.add(Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu'))
+model.add(Conv2D(32, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu'))
 # convolutional layer
-model.add(Conv2D(128, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
-model.add(MaxPooling2D(pool_size=(2,2))) #Compress filtered data for image and reinforce patterns
+model.add(Conv2D(32, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
+model.add(MaxPooling2D(pool_size=(2,2), padding='same')) #Compress filtered data for image and reinforce patterns
+
+model.add(Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
+model.add(MaxPooling2D(pool_size=(2,2), padding = 'same')) #Compress filtered data for image and reinforce patterns
 
 model.add(Conv2D(128, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
-model.add(MaxPooling2D(pool_size=(2,2))) #Compress filtered data for image and reinforce patterns
-model.add(Dropout(0.25)) #Remove useless filtered images and reduce training time
+
+model.add(Conv2D(256, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
+
+model.add(Conv2D(512, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
+
 # flatten output of conv
 model.add(Flatten()) #Flattens all tensor with all the 
 
